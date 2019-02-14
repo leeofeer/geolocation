6209baa8b00a06c517269a70d46bb0679bc85291
leeo <leeo.feer@gmail.com>
2019-02-14 14:25:03 -0400

    v1

---
+++ ubicaciones.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.36-MariaDB : Database - geolocalizar
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`geolocalizar` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `geolocalizar`;
+
+/*Table structure for table `ubicacion` */
+
+DROP TABLE IF EXISTS `ubicacion`;
+
+CREATE TABLE `ubicacion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `negocio` char(40) DEFAULT NULL,
+  `latitud` double DEFAULT NULL,
+  `longitud` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+/*Data for the table `ubicacion` */
+
+insert  into `ubicacion`(`id`,`negocio`,`latitud`,`longitud`) values (1,'rio paraguay',-25.284748178536493,-57.67324447631837),(2,'nanawa',-25.280557285531962,-57.703971862792976),(4,'coordillera, py',-25.29437116258816,-56.83227539062501),(6,'asuncion',-25.195000424307494,-57.63427734375001);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
